5c17a3f7d20244b6ae5d9db48261fd5a7d7ab5ea
Filip Kania <livarion@gmail.com>
2017-05-15 13:17:33 +0200

    project basic setup

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Very Unique And Catchy Company Name</title>
+	<link rel="stylesheet" type="text/css" href="style/app.css">
+</head>
+<body>
+	<div id="wdw-index" class="wdw">
+		
+	</div>
+	<div id="wdw-event" class="wdw">
+		
+	</div>
+	<div id="wdw-about" class="wdw">
+		
+	</div>
+	<div id="wdw-contact" class="wdw">
+		
+	</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script>
+	
+</script>
+</body>
+</html>
